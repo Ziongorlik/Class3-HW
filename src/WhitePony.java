d6212f879b0d8699b070aa5826b99a393a72db04
Zion Gorlik <78359133+Ziongorlik@users.noreply.github.com>
2021-04-16 20:39:17 +0300

    implemented the IsRunnable interface

--- src/WhitePony.java
+++ src/WhitePony.java
@@ -1,10 +1,15 @@
 // Exercise-5
-public class WhitePony extends Pony {
+public class WhitePony extends Pony implements IsRunnable{
     public WhitePony(String name) {
         super(name);
     }
 
     public String getPonyColor() {
         return "White";
+    }
+    
+    @Override
+    public void run(){
+        System.out.println("I'm running");
     }
 }
